a50cf8611228aacc3dfb61f44113253973ffd521
Mithlesh Sharma <itsyour.rohan@gmail.com>
2024-03-14 01:27:38 +0530

    Changed proxy from cros proxy to thingproxy

--- src/utils/useMobileMenu.js
+++ src/utils/useMobileMenu.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {useState,useEffect} from 'react'
 const useMobileMenu = (resId) => {
-    const FETCH_MENU_URL_MOBILE =
+    const FETCH_MENU_URL_MOBILE1 =
     "https://corsproxy.org/?"+encodeURIComponent("https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&submitAction=ENTER&restaurantId="+resId);
+    const FETCH_MENU_URL_MOBILE ="https://thingproxy.freeboard.io/fetch/"+encodeURIComponent("https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&submitAction=ENTER&restaurantId="+resId);
     const [mobilemenu,setmobilemenu]=useState([])
       async function mob(){
          let data;
