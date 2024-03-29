1237effafe4fb469bcd9a27520a598ed37e16280
Mithlesh Sharma <itsyour.rohan@gmail.com>
2024-03-29 20:28:58 +0530

    updated CDN_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const CDN_URL =
-  'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/';
+  'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
 
 export const LOGO_URL = './src/assets/logo.jpg'
   // export const MENU_API ="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=31.2920743&lng=75.5945828&restaurantId=";
