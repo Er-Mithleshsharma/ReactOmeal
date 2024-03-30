99e394ac2b7614b506376beeb3a8309653900f88
Mithlesh Sharma <itsyour.rohan@gmail.com>
2024-03-30 15:21:05 +0530

    Lazy loading

--- src/components/RoutingConfig.jsx
+++ src/components/RoutingConfig.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense , lazy } from 'react'
 import App from '../App.jsx'
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-import Contact from './contact.jsx'
+const Contact = lazy(() => import('./contact'));
 const About = lazy(() => import('./About'));
 import Body from './Body.jsx'
 import RestaurantMenu from './RestaurantMenu.jsx'
@@ -34,7 +34,9 @@ const appRouter = createBrowserRouter([
     {
       path :"/contact",
       element:
+      <Suspense>
          <Contact/>
+         </Suspense>
      
     },
     {
